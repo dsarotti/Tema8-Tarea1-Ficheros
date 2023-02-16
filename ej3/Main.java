4373d58600e7c540a978d4153a9675a00d99d9f2
dsarotti <sr.sarotti@gmail.com>
2023-02-16 22:33:42 +0100

    cambios

--- ej3/Main.java
+++ ej3/Main.java
@@ -6,21 +6,37 @@ import java.io.Console;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 class Main {
     public static void main(String[] args) {
         Console c = System.console();
         String nombreArchivo;
         nombreArchivo = c.readLine("escribe el nombre del archivo a editar: ");
-        String texto = leer(nombreArchivo);
         String temp;
-        temp = c.readLine("Escribe lineas de texto separadas por <enter>, linea vacía para terminar: ") + "\n";
+        List<String> lines = new LinkedList<>();
+        temp = c.readLine("Escribe lineas de texto separadas por <enter>, linea vacía para terminar: ");
         while (!temp.isBlank()) {
-            texto += temp;
-            writeTextToFile(nombreArchivo, texto);
-            temp = c.readLine() + "\n";
+            lines.add(temp);
+            temp = c.readLine("Escribe lineas de texto separadas por <enter>, linea vacía para terminar: ");
         }
+        appendLinesToFile(nombreArchivo, lines);
         System.out.println("edición finalizada, el resultado es el siguiente: \n" +leer(nombreArchivo));
+    }
+
+    public static void appendLinesToFile(String fn, List<String> lines){
+        for (String txt : lines) {
+            appendTextToFile(fn, "\n" + txt);
+        }
+    }
+
+    public static void appendTextToFile(String fn, String txt){
+        try (FileWriter archivo = new FileWriter(fn, true);BufferedWriter writer = new BufferedWriter(archivo)) {
+            writer.write(txt);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void writeTextToFile(String fn, String txt) {
